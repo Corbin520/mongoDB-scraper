48bf89a8644f09d420841fb702dc08755ac9b8d9
Corbin Brockbank <corbinbrckbnk76@gmail.com>
2019-06-13 12:08:32 -0600

    displaying as buttons from DB

--- public/app.js
+++ public/app.js
@@ -1,12 +1,21 @@
-alert("Page worked")
+
 
 $("#scrape-button").on("click", function(){
-    alert("You clicked me")
+
+    // or do a EACH statement for handlebars
+    $.getJSON("/all", function(data) {
+        // For each one
+        for (var i = 0; i < data.length; i++) {
+          // Display the apropos information on the page
+          $("#news-articles").append("<button><p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].summary + "<br />" + data[i].link + "</p><br></button");
+        }
+      });
+      $("#show-card").show()
+});
+
+$("#clear-button").on("click", function() {
+    // * We drop a collection with `db.[COLLECTION_NAME].drop()`
+    $.getJson("/all", function(data) {
+        db.News.drop()
+    })
 })
-$.getJSON("/all", function(data) {
-    // For each one
-    for (var i = 0; i < data.length; i++) {
-      // Display the apropos information on the page
-      $("#news-articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].summary + "<br />" + data[i].link + "</p>");
-    }
-  });
