89629948fd8c7dfb9198b65a562f177ef6ef6ec1
Corbin Brockbank <corbinbrckbnk76@gmail.com>
2019-06-15 13:22:38 -0600

    ....asdfas

--- server.js
+++ server.js
@@ -121,6 +121,6 @@ app.get("/get/saved", function(req, res) {
 })
 
 // Starting the server on PORT
-app.listen(process.env.PORT || 8080, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
